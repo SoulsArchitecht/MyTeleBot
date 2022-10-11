b0766cc63567aa86740b6af54c8e0f6c8ae556d6
Serge <sshibkojavadev@gmail.com>
2022-10-11 21:28:35 +0800

    basic-configuration

---
+++ src/main/java/ru/sshibko/TeleBot/TeleBotApplication.java
@@ -0,0 +1,13 @@
+package ru.sshibko.TeleBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeleBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeleBotApplication.class, args);
+	}
+
+}
---
+++ src/main/java/ru/sshibko/TeleBot/config/BotInitializer.java
@@ -0,0 +1,23 @@
+package ru.sshibko.TeleBot.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+import ru.sshibko.TeleBot.service.TelegramBotService;
+
+@Configuration
+public class BotInitializer {
+
+    @Autowired
+    TelegramBotService bot;
+
+    @EventListener({ContextRefreshedEvent.class})
+    public void init() throws TelegramApiException {
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+        telegramBotsApi.registerBot(bot);
+    }
+}
